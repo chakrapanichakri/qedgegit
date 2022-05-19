8bee36424aca849896319d6cdb1682f742ba2ccd
chakri <chakrapani427@gmail.com>
2022-05-19 15:51:21 +0530

    new changes done by chakri developer

--- application.java
+++ application.java
@@ -12,5 +12,24 @@ class Bike extends Vehicle{
   Bike obj = new Bike();  
   //calling the method with child class instance  
   obj.run();  
-  }  
+  }
+#new changes done by chakri
+public class CopyArray {    
+    public static void main(String[] args) {        
+             //Initialize array     
+        int [] arr1 = new int [] {1, 2, 3, 4, 5};     
+         //Create another array arr2 with size of arr1    
+        int arr2[] = new int[arr1.length];    
+        //Copying all elements of one array into another    
+        for (int i = 0; i < arr1.length; i++) {     
+            arr2[i] = arr1[i];     
+        }      
+         //Displaying elements of array arr1     
+        System.out.println("Elements of original array: ");    
+        for (int i = 0; i < arr1.length; i++) {     
+           System.out.print(arr1[i] + " ");    
+        }     
+            
+        System.out.println(); 
+  
 }  
